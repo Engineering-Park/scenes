d305e925cc3e041339c5281f1484a121f4a15bd7
Tristan Martindale <tristanmartindale@gmail.com>
2022-03-17 21:31:05 +0100

    added parachute scene

---
+++ parachute/src/game.ts
@@ -0,0 +1,32 @@
+import { addSound } from "@engineeringpark/components";
+import { addAttribution, createGltfShape } from "@engineeringpark/entities";
+import { FlightSystem } from "@engineeringpark/systems";
+
+const scene = new Entity("scene_parachute");
+engine.addEntity(scene);
+
+const floor = createGltfShape({
+  model: "FloorBaseGrass_01/FloorBaseGrass_01.glb",
+  name: "floor",
+  position: new Vector3(8, 0, 8)
+});
+floor.setParent(scene);
+
+const parachute = createGltfShape({
+  model: "parachute.glb",
+  name: "Parachute",
+  position: new Vector3(8, 1, 8),
+  scale: new Vector3(0.25, 0.25, 0.25)
+});
+parachute.setParent(scene);
+
+addAttribution({
+  entity: parachute,
+  text: "CC-BY: Poly by Google",
+  position: new Vector3(0, 0, -2),
+  rotation: Quaternion.Euler(0, -90, 0)
+});
+
+addSound({ entity: parachute, sound: "parachute.mp3" });
+
+engine.addSystem(new FlightSystem(parachute, -0.5, 3));
