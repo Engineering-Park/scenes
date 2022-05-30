d74497a43ad10612055046b14236a4412c4120dc
Tristan Martindale <tristanmartindale@gmail.com>
2022-05-30 23:08:50 +0200

    updated decentraland-ecs version

---
+++ update-dcl.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+npm install -g decentraland
+
+cd aircraft
+npm install decentraland-ecs@latest
+cd ..
+
+cd bicycle
+npm install decentraland-ecs@latest
+cd ..
+
+cd heaviside_layer
+npm install decentraland-ecs@latest
+cd ..
+
+cd lib
+npm install decentraland-ecs@latest
+cd ..
+
+cd parachute
+npm install decentraland-ecs@latest
+cd ..
+
+cd placeholder_1x1
+npm install decentraland-ecs@latest
+cd ..
+
+cd railway
+npm install decentraland-ecs@latest
+cd ..
+
+cd stephenson_park
+npm install decentraland-ecs@latest
+cd ..
+
+cd systems_engineering
+npm install decentraland-ecs@latest
+cd ..
+
+cd wright_way_north
+npm install decentraland-ecs@latest
+cd ..
+
+cd wright_way_south
+npm install decentraland-ecs@latest
+cd ..
